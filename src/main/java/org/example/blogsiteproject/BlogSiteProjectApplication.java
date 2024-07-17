5fba6ea1e24af7403648edfce6aca72622445756
yusuf özbay <ozbay9985@gmail.com>
2024-07-17 15:02:26 +0300

    first commit

---
+++ src/main/java/org/example/blogsiteproject/BlogSiteProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.blogsiteproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSiteProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSiteProjectApplication.class, args);
+	}
+
+}

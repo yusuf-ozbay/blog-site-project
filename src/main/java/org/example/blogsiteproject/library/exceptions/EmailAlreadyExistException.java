192719582e952ed80c8ad5395509b555b57e167d
yusuf özbay <ozbay9985@gmail.com>
2024-08-24 21:13:43 +0300

    exception sınıfları eklendi

---
+++ src/main/java/org/example/blogsiteproject/library/exceptions/EmailAlreadyExistException.java
@@ -0,0 +1,14 @@
+package org.example.blogsiteproject.library.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class EmailAlreadyExistException extends RuntimeException{
+
+    private String message;
+    public EmailAlreadyExistException(String message){
+        super(message);
+    }
+
+}
---
+++ src/main/java/org/example/blogsiteproject/library/exceptions/ErrorDetails.java
@@ -0,0 +1,19 @@
+package org.example.blogsiteproject.library.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetails {
+    private LocalDateTime timestamp;
+    private String message;
+    private String path;
+    private String errorCode;
+}
